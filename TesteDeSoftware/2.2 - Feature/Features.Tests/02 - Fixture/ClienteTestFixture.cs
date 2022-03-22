d3ba640f5599b81de65e3223ac2dfde81893f485
Farley Rufino <farley.rufino@apinteractive.com.br>
2022-03-22 00:54:41 -0300

    Bogus

--- TesteDeSoftware/2.2 - Feature/Features.Tests/02 - Fixture/ClienteTestFixture.cs
+++ TesteDeSoftware/2.2 - Feature/Features.Tests/02 - Fixture/ClienteTestFixture.cs
@@ -1,4 +1,6 @@
-﻿using Features.Clientes;
+﻿using Bogus;
+using Bogus.DataSets;
+using Features.Clientes;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -17,15 +19,35 @@ namespace Features.Tests._02___Fixture
     {
         public Cliente gerarClienteValido()
         {
-            var cliente = new Cliente(
-               Guid.NewGuid(),
-               "Eduardo",
-               "Pires",
-               DateTime.Now.AddYears(-30),
-               "edu@edu.com",
-               true,
-               DateTime.Now
-               );
+            var genero = new Faker().PickRandom<Name.Gender>();
+
+            var cliente = new Faker<Cliente>("pt_BR")
+                .CustomInstantiator( f => new Cliente(
+                     Guid.NewGuid(),
+                     f.Name.FirstName(genero),
+                     f.Name.LastName(genero),
+                     f.Date.Past(80,DateTime.Now.AddYears(-18)),
+                     "",
+                     true,
+                     DateTime.Now))
+                .RuleFor(c => c.Email,(f,c)=> f.Internet.Email(
+                    c.Nome.ToLower(),
+                    c.Sobrenome.ToLower()));
+
+
+            //var email = new Faker().Internet.Email("Eduardo","Pires","gamil");
+            //var clienteFaker = new Faker<Cliente>();
+            //clienteFaker.RuleFor(c => c.Nome, (f, c) => f.Name.FirstName());
+
+            //var cliente = new Cliente(
+            //   Guid.NewGuid(),
+            //   "Eduardo",
+            //   "Pires",
+            //   DateTime.Now.AddYears(-30),
+            //   "edu@edu.com",
+            //   true,
+            //   DateTime.Now
+            //   );
 
             return cliente;
         }
