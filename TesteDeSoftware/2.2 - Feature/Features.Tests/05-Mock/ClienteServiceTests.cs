0484ab33261fcb105c1d5e8dcccfd7f0f5dc4b3d
Farley Rufino <farley.rufino@apinteractive.com.br>
2022-03-22 01:51:34 -0300

    Realizando mock de objetos

---
+++ TesteDeSoftware/2.2 - Feature/Features.Tests/05-Mock/ClienteServiceTests.cs
@@ -0,0 +1,70 @@
+﻿using Features.Clientes;
+using MediatR;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Xunit;
+
+namespace Features.Tests._05_Mock
+{
+    [Collection(nameof(ClienteBogusCollection))]
+    public class ClienteServiceTests
+    {
+        readonly ClienteTestsBogusFixture _clienteTestsBogus;
+
+        public ClienteServiceTests(ClienteTestsBogusFixture clienteTestsFixture)
+        {
+            _clienteTestsBogus = clienteTestsFixture;
+        }
+
+        [Fact(DisplayName = "Adicionar Cliente com Sucesso")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_Adicionar_DeveExecutarComSucesso()
+        {
+            // Arrange
+            var cliente = _clienteTestsBogus.GerarClienteValido();
+            var clienteRepo = new Mock<IClienteRepository>();
+            var mediator = new Mock<IMediator>();
+            var clienteService = new ClienteService(clienteRepo.Object, mediator.Object);
+
+            // Act
+            clienteService.Adicionar(cliente);
+
+            // Assert
+            Assert.True(cliente.EhValido());
+            clienteRepo.Verify(r => r.Adicionar(cliente),Times.Once); // faz um asseert no metodo
+            mediator.Verify(m => m.Publish(It.IsAny<INotification>(),CancellationToken.None),Times.Once); // Passou pelo metodo
+        }
+
+        [Fact(DisplayName = "Adicionar Cliente com Falha")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_Adicionar_DeveFalharDevidoClienteInvalido()
+        {
+            // Arrange
+           
+
+           ;
+
+            // Act
+            
+
+            // Assert
+           
+        }
+
+        [Fact(DisplayName = "Obter Clientes Ativos")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_ObterTodosAtivos_DeveRetornarApenasClientesAtivos()
+        {
+            // Arrange
+          
+            // Act
+          
+
+            // Assert 
+          
+        }
+    }
+}
