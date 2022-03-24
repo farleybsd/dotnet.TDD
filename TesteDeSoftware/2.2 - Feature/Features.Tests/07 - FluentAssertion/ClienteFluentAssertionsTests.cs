06f8a1e517cbb8fdc025e1fe1743f033ba908fea
Farley Rufino <farley.rufino@apinteractive.com.br>
2022-03-24 01:03:46 -0300

    Utilizando Fluent Assertions

---
+++ TesteDeSoftware/2.2 - Feature/Features.Tests/07 - FluentAssertion/ClienteFluentAssertionsTests.cs
@@ -0,0 +1,90 @@
+﻿using Features.Clientes;
+using Features.Tests._06___Auto_Mock;
+using FluentAssertions;
+using MediatR;
+using Moq;
+using Moq.AutoMock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Features.Tests._07___FluentAssertion
+{
+    [Collection(nameof(ClienteBogusCollection))]
+    public class ClienteFluentAssertionsTests
+    {
+        private readonly ClienteTestsAutoMockerFixture _clienteTestsFixture;
+        readonly ITestOutputHelper _outputHelper;
+
+
+        public ClienteFluentAssertionsTests(ClienteTestsAutoMockerFixture clienteTestsFixture,
+            ITestOutputHelper outputHelper)
+        {
+            _clienteTestsFixture = clienteTestsFixture;
+        }
+
+        [Fact(DisplayName = "Adicionar Cliente com Sucesso")]
+        [Trait("Categoria", "Cliente Service AutoMock Tests")]
+        public void ClienteService_Adicionar_DeveExecutarComSucesso()
+        {
+            // Arrange
+            var cliente = _clienteTestsFixture.GerarClienteValido();
+
+            // Act
+            var result = cliente.EhValido();
+
+            //Assert
+            //Assert.True(result);
+            //Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+
+            // Assert 
+
+            result.Should().BeTrue();
+            cliente.ValidationResult.Errors.Should().HaveCount(0);
+        }
+
+        [Fact(DisplayName = "Adicionar Cliente com Falha")]
+        [Trait("Categoria", "Cliente Service AutoMock Tests")]
+        public void ClienteService_Adicionar_DeveFalharDevidoClienteInvalido()
+        {
+            // Arrange
+            var cliente = _clienteTestsFixture.GerarClienteInvalido();
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert 
+            result.Should().BeFalse();
+            cliente.ValidationResult.Errors.Should().HaveCountGreaterOrEqualTo(1, "deve possuir erros de validação");
+
+            _outputHelper.WriteLine($"Foram encontrados {cliente.ValidationResult.Errors.Count} erros nesta validação");
+
+        }
+
+        [Fact(DisplayName = "Obter Clientes Ativos")]
+        [Trait("Categoria", "Cliente Service AutoMock Tests")]
+        public void ClienteService_ObterTodosAtivos_DeveRetornarApenasClientesAtivos()
+        {
+            // Arrange
+            var mocker = new AutoMocker();
+
+            var clienteService = mocker.CreateInstance<ClienteService>();
+
+            mocker.GetMock<IClienteRepository>().Setup(c => c.ObterTodos()).Returns(_clienteTestsFixture.ObterClientesVariados);
+
+
+            // Act
+            var clientes = clienteService.ObterTodosAtivos();
+
+            // Assert 
+            mocker.GetMock<IClienteRepository>().Verify(r => r.ObterTodos(), Times.Once);
+            Assert.True(clientes.Any());
+            Assert.False(clientes.Count(c => !c.Ativo) > 0);
+
+        }
+    }
+}
---
+++ TesteDeSoftware/2.2 - Feature/Features.Tests/07 - FluentAssertion/ClienteServiceFluentAssertionTests.cs
@@ -0,0 +1,77 @@
+﻿using Features.Clientes;
+using Features.Tests._06___Auto_Mock;
+using FluentAssertions;
+using MediatR;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Xunit;
+
+namespace Features.Tests._07___FluentAssertion
+{
+    [Collection(nameof(ClienteAutoMockerCollection))]
+    public class ClienteServiceFluentAssertionTests
+    {
+        readonly ClienteTestsAutoMockerFixture ClienteTestAutoMockerFixture;
+        private readonly ClienteService _clienteService;
+
+        public ClienteServiceFluentAssertionTests(ClienteTestsAutoMockerFixture clienteTestsFixture)
+        {
+            ClienteTestAutoMockerFixture = clienteTestsFixture;
+            _clienteService = ClienteTestAutoMockerFixture.ObterClienteService();
+        }
+
+
+        [Fact(DisplayName = "Adicionar Cliente com Sucesso")]
+        [Trait("Categoria", "Cliente Service AutoMockFixture Tests")]
+        public void ClienteService_Adicionar_DeveExecutarComSucesso()
+        {
+            // Arrange
+            var cliente = ClienteTestAutoMockerFixture.GerarClienteValido();
+
+            // Act
+            _clienteService.Adicionar(cliente);
+
+            // Assert
+            cliente.EhValido().Should().BeTrue();
+            ClienteTestAutoMockerFixture.Mocker.GetMock<IClienteRepository>().Verify(r => r.Adicionar(cliente), Times.Once); // faz um asseert no metodo
+            ClienteTestAutoMockerFixture.Mocker.GetMock<IMediator>().Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Once); // Passou pelo metodo
+        }
+
+        [Fact(DisplayName = "Adicionar Cliente com Falha")]
+        [Trait("Categoria", "Cliente Service AutoMockFixture Tests")]
+        public void ClienteService_Adicionar_DeveFalharDevidoClienteInvalido()
+        {
+            // Arrange
+            var cliente = ClienteTestAutoMockerFixture.GerarClienteInvalido();
+
+            // Act
+            _clienteService.Adicionar(cliente);
+
+            // Assert
+            cliente.EhValido().Should().BeFalse("Possui inconsistências");
+            ClienteTestAutoMockerFixture.Mocker.GetMock<IClienteRepository>().Verify(r => r.Adicionar(cliente), Times.Never); // faz um asseert no metodo
+            ClienteTestAutoMockerFixture.Mocker.GetMock<IMediator>().Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Never); // Passou pelo metodo
+
+        }
+
+        [Fact(DisplayName = "Obter Clientes Ativos")]
+        [Trait("Categoria", "Cliente Service AutoMockFixture Tests")]
+        public void ClienteService_ObterTodosAtivos_DeveRetornarApenasClientesAtivos()
+        {
+            // Arrange
+            ClienteTestAutoMockerFixture.Mocker.GetMock<IClienteRepository>().Setup(c => c.ObterTodos()).Returns(ClienteTestAutoMockerFixture.ObterClientesVariados);
+            // Act
+            var clientes = _clienteService.ObterTodosAtivos();
+            // Assert 
+            ClienteTestAutoMockerFixture.Mocker.GetMock<IClienteRepository>().Verify(r => r.ObterTodos(), Times.Once);
+            clientes.Should().HaveCountGreaterOrEqualTo(1).And.OnlyHaveUniqueItems();
+            clientes.Should().NotContain(c => !c.Ativo);
+
+        }
+    }
+}
+
