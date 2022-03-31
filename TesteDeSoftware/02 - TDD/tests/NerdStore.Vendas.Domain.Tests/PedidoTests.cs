22ad9826611271e20c721c143662886a0017125d
Farley Rufino <farley.rufino@apinteractive.com.br>
2022-03-31 00:57:40 -0300

    pedido-remover-item

--- TesteDeSoftware/02 - TDD/tests/NerdStore.Vendas.Domain.Tests/PedidoTests.cs
+++ TesteDeSoftware/02 - TDD/tests/NerdStore.Vendas.Domain.Tests/PedidoTests.cs
@@ -155,5 +155,42 @@ namespace NerdStore.Vendas.Domain.Tests
             Assert.Throws<DomainException>(() => pedido.AtualizarItem(pedidoItemAtualizado));
 
         }
+
+        [Fact(DisplayName = "Remover Item Pedido Inexistente")]
+        [Trait("Categoria", "Vendas - Pedido")]
+        public void RemoverItemPedido_ItemNaoExisteNaLista_DeveRetornarUmaaException()
+        {
+            //Arrange 
+            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
+            var pedidoItemRemover = new PedidoItem(Guid.NewGuid(), "Produto Teste", 5, 100);
+
+            //Act e Assert
+            Assert.Throws<DomainException>(() => pedido.RemoverItem(pedidoItemRemover));
+        }
+
+        [Fact(DisplayName = "Remover Item Pedido Deve Calcular Valor Total ")]
+        [Trait("Categoria", "Vendas - Pedido")]
+        public void RemoverItemPedido_ItemExistente_DeveAtualizarValorTotal()
+        {
+            // Arrange
+            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
+            var productId = Guid.NewGuid();
+            var pedidoItem1 = new PedidoItem(Guid.NewGuid(), "Produto Xpto", 2, 100);
+            var pedidoItem2 = new PedidoItem(productId, "Produto Teste", 3, 15);
+
+            pedido.AdicionarItem(pedidoItem1);
+            pedido.AdicionarItem(pedidoItem2);
+
+
+            var totalPedido = pedidoItem2.Quantidade * pedidoItem2.ValorUnitario;
+
+            //Act
+            pedido.RemoverItem(pedidoItem1);
+            //Assert
+            Assert.Equal(totalPedido, pedido.ValorTotal);
+
+
+        }
+
     }
 }
