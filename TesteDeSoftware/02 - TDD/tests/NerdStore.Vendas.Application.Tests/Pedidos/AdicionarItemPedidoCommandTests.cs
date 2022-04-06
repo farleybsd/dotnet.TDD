0bfee7e85e0a0895789088b4e9b1c709e8c35115
Farley Rufino <farley.rufino@apinteractive.com.br>
2022-04-06 02:09:17 -0300

    Pedido - Adicionar Item Command

---
+++ TesteDeSoftware/02 - TDD/tests/NerdStore.Vendas.Application.Tests/Pedidos/AdicionarItemPedidoCommandTests.cs
@@ -0,0 +1,25 @@
+﻿using NerdStore.Vendas.Application.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NerdStore.Vendas.Application.Tests.Pedidos
+{
+    public class AdicionarItemPedidoCommandTests
+    {
+        [Fact(DisplayName = "Adicionar Item Command Vàlido ")]
+        [Trait("Categoria", "Vendas - Pedido Commads")]
+        public void AdicionarItemPedidoCommand_CommandoEstadoValido_DevePassarNaValidacao()
+        {
+            //Arrange
+            var pedidoCommand = new AdicionarItemPedidoCommand(Guid.NewGuid(), Guid.NewGuid(), "Produto Teste", 2, 100);
+
+            //Act
+            var result = pedidoCommand.EhValido();
+
+            //Assert
+            Assert.True(result);
+        }
+    }
+}
